2587ac863cf15dccd32c947ac0c552845c5ab15f
sanechabik <sanechabik@mail.ru>
2023-03-13 15:43:40 +0300

    добавил equals/hashCode в класс Book

--- src/lesson13/Book.java
+++ src/lesson13/Book.java
@@ -1,6 +1,21 @@
 package lesson13;
 
+import java.util.Objects;
+
 public class Book {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return writingDate == book.writingDate && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, writingDate);
+    }
+
     @Override
     public String toString() {
         return "Book{" +
